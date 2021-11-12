b79e39f088de4613b97f694e31ea3c1149a1736c
Blake Wood Jr <blake.wood.bst@visible.edu>
2021-11-11 18:44:06 -0800

    added bare bones features

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
+/** @jsx jsx */ /** @jsxRuntime classic */ import { jsx } from "theme-ui";
+import React, { useState } from 'react'
 import './App.css';
+import { Button } from 'react-bootstrap'
+
+const options = [
+  {
+    name: 'Option 1'
+  },
+  {
+    name: 'Option 2'
+  },
+  {
+    name: 'Option 3'
+  },
+  {
+    name: 'Option 4'
+  }
+]
+
+const menuTheme = {
+  height: '350px', 
+  width: '250px', 
+  border: '1px solid orange',
+  '.menu-item': {
+    cursor: 'pointer'
+  },
+  '.menu-item:hover': {
+    backgroundColor: 'orange'
+  }
+}
+
+const Menu = () => {
+
+  const handleOnClick = (e) => {
+    console.log(e)
+  }
+
+  return (
+    <div sx={menuTheme}>
+      {
+        options.map(item => {
+          return (
+            <div
+              onClick={() => handleOnClick(item)}
+              className="menu-item"
+            >{item.name}</div>
+          )
+        })
+      }
+    </div>
+  )
+}
 
 function App() {
+
+  const [xPos, setXPos] = useState(0)
+  const [yPos, setYPos] = useState(0)
+  const [showMenu, setShowMenu] = useState(false)
+
+  const handleContextMenu = (e) => {
+    e.preventDefault()
+    setXPos(e.clientX)
+    setYPos(e.clientY)
+    setShowMenu(true)
+  }
+  
+  const handleOnClick = (e) => {
+    setShowMenu(false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onContextMenu={handleContextMenu} onClick={handleOnClick}>
+      <div className="container">
+        Right Click to Open Menu
+      </div>
+      { 
+        showMenu && (
+          <div style={{position:'absolute', zIndex: '100', top: yPos, left: xPos}}>
+            <Menu />
+          </div>
+        )
+      }
     </div>
   );
 }
